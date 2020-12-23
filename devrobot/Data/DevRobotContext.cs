88de5e2378313f561f2bc8127fe07423fd9a78e4
Renato de Azevedo Lacerda <renatolacerda@matur.com.br>
2020-12-23 09:50:02 -0300

    conexao com o banco de dados

---
+++ devrobot/Data/DevRobotContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using devrobot.Models;
+
+namespace devrobot.Data
+{
+    public class DevRobotContext : DbContext
+    {
+        public DevRobotContext(DbContextOptions<DevRobotContext> options) : base(options) { }
+        public DbSet<devrobot.Models.Autenticacao> Autenticacao { get; set; }
+        //public DbSet<Usuario> Usuario { get; set; }
+
+
+    }
+}
---
+++ devrobot/Models/Autenticacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devrobot.Models
+{
+    public class Autenticacao
+    {
+        [Key]
+        public int Id { get; set; }
+        public string  User { get; set; }
+        public string Senha { get; set; }
+    }
+}
