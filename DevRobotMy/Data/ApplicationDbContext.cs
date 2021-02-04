304905b9eec78a2153b960d2514e2fb63c5cff5c
Renato de Azevedo Lacerda <renatolacerda@matur.com.br>
2021-02-04 15:39:21 -0300

    autenticacao

---
+++ DevRobotMy/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DevRobotMy.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevRobotMy.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Autenticacao> Autenticacao { get; set; }
+    }
+}
---
+++ DevRobotMy/Models/Autenticacao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevRobotMy.Models
+{
+    public class Autenticacao
+    {
+        public int Id { get; set; }
+        public string  user { get; set; }
+        public string  senha { get; set; }
+        public string NomeEmpresa { get; set; }
+
+        public Autenticacao()
+        {
+        }
+
+        public Autenticacao(int id, string user, string senha, string nomeEmpresa)
+        {
+            Id = id;
+            this.user = user;
+            this.senha = senha;
+            NomeEmpresa = nomeEmpresa;
+        }
+    }
+}
