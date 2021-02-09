10fac67da82162ac90946c57a161374f245c3f88
Renato de Azevedo Lacerda <renatolacerda@matur.com.br>
2021-02-09 08:58:20 -0300

    lol

--- DevRobotMy/Startup.cs
+++ DevRobotMy/Startup.cs
@@ -95,7 +95,8 @@ namespace DevRobotMy
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
